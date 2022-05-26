114db7721d84c07dee3236f04cfbf7e592e81ee6
Thomas Nuyken <thomas.nuyken@proton.me>
2022-05-26 14:55:19 +0200

    Add initial K6 test files

---
+++ tests/small-post.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+
+export let options = {
+    insecureSkipTLSVerify: true,
+    noConnectionReuse: false,
+    discardResponseBodies: true,
+    scenarios: {
+        constantRate: {
+            executor: 'per-vu-iterations',
+            vus: 30,
+            iterations: 1000,
+            maxDuration: '1h',
+        }
+    }
+};
+
+export default () => {
+    const carsUrl = 'http://localhost:8090/api/cars';
+    const params = {
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    };
+    const car = JSON.stringify({
+        name: "name"
+    });
+    http.post(carsUrl, car, params);
+};
