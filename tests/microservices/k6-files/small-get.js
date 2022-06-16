58c3e8338728695355be7b9b7f83bb4d5b8a84fe
Thomas Nuyken <thomas.nuyken@proton.me>
2022-06-16 15:46:36 +0200

    Add convenience scripts

--- tests/microservices/k6-files/small-get.js
+++ tests/microservices/k6-files/small-get.js
@@ -16,7 +16,7 @@ export let options = {
 
 export default () => {
     http.batch([
-        ['GET', 'http://localhost/consumptions' ],
-        ['GET', 'http://localhost/cars' ]
+        ['GET', 'http://localhost/consumptions/e00613c8-9c30-48ed-82b9-31bdbaea6d1d' ],
+        ['GET', 'http://localhost/cars/4d246d4d-dfa3-45ef-92c7-e3f731641012' ]
     ]);
 };
