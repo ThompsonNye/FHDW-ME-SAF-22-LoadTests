925ac07b177ba689555fd7d0b3789e07a9a02a42
Thomas Nuyken <thomas.nuyken@proton.me>
2022-06-16 16:13:21 +0200

    Improve scripts and set LF line endings

--- tests/monolith/k6-files/large-post.js
+++ tests/monolith/k6-files/large-post.js
@@ -3,7 +3,6 @@ import http from 'k6/http';
 export let options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
-    discardResponseBodies: true,
     scenarios: {
         constantRate: {
             executor: 'per-vu-iterations',
