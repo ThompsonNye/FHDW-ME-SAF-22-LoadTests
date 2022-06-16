d09fb4b4e9eb67927a01561746a4dd82673e98f0
Thomas Nuyken <thomas.nuyken@proton.me>
2022-06-16 15:07:01 +0200

    Update tests to include more actions

--- tests/monolith/small-post.js
+++ tests/monolith/small-post.js
@@ -3,7 +3,6 @@ import http from 'k6/http';
 export let options = {
     insecureSkipTLSVerify: true,
     noConnectionReuse: false,
-    discardResponseBodies: true,
     scenarios: {
         constantRate: {
             executor: 'per-vu-iterations',
@@ -15,14 +14,20 @@ export let options = {
 };
 
 export default () => {
-    const carsUrl = 'http://localhost/api/cars';
     const params = {
         headers: {
             'Content-Type': 'application/json',
         },
     };
     const car = JSON.stringify({
-        name: "name"
+        name: "car"
     });
-    http.post(carsUrl, car, params);
+    const response = http.post('http://localhost/api/cars', car, params);
+    const consumption = JSON.stringify({
+        DateTime: "2022-06-01T19:22:00+02:00",
+        Distance: 1,
+        Amount: 1,
+        carId: response.json().id
+    });
+    http.post('http://localhost/api/consumptions', consumption, params);
 };
