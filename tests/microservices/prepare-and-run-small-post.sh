61a20718673be73613b073243b5774234befff70
Thomas Nuyken <thomas.nuyken@proton.me>
2022-06-18 14:03:52 +0200

    Fix scripts for multi db scenarios

--- tests/microservices/prepare-and-run-small-post.sh
+++ tests/microservices/prepare-and-run-small-post.sh
@@ -1,5 +1,5 @@
-PGPASSWORD=password psql -h localhost -U postgres -c "DELETE FROM \"Cars\";"
+PGPASSWORD=password psql -h localhost -U postgres -p 55432 -c "DELETE FROM \"Cars\";"
 PGPASSWORD=password psql -h localhost -U postgres -c "DELETE FROM \"Consumptions\";"
 k6 run k6-files/small-post.js
-PGPASSWORD=password psql -h localhost -U postgres -c "DELETE FROM \"Cars\";"
+PGPASSWORD=password psql -h localhost -U postgres -p 55432 -c "DELETE FROM \"Cars\";"
 PGPASSWORD=password psql -h localhost -U postgres -c "DELETE FROM \"Consumptions\";"
